610438f0c1894b093d48c82092d0b62e780b2655
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2023-12-15 16:48:09 +0000

    Created results component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header.jsx";
 import Input from "./components/Input.jsx";
+import Results from "./components/Results.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <Input />
+      <Results />
     </>
   );
 }
---
+++ src/components/Results.jsx
@@ -0,0 +1,33 @@
+// import calculateInvestmentResults from "./util/investment.js";
+
+export default function Results() {
+  return (
+    <div id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Intrest(Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>2012</td>
+          <td>20000</td>
+          <td>2020</td>
+          <td>5000</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>2012</td>
+          <td>20000</td>
+          <td>2020</td>
+          <td>5000</td>
+          <td>2</td>
+        </tr>
+      </tbody>
+    </div>
+  );
+}
